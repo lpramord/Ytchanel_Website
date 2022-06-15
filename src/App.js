88f3d2d393ab2e318a9ae261f2af0299684fb076
lpramord <lpramord@gmail.com>
2022-06-15 19:57:51 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React , {useEffect, useState }  from 'react';
-import ustube from "usetube"
 import './App.css';
 
 const App = () => {
   
   const [videos , setVideos] = useState([])
   const [chanelDiscription , setChanelDiscription] = useState([])
-  const [chanelName , setChaneName] = useState([])
-  const [chanelLogo , setChanelLogo] = useState([])
+  const [chanellogo , setChanellogo] = useState([])
 
   useEffect(()=>{
     getData()
@@ -17,24 +15,30 @@ const App = () => {
   const chanelId = "UCJA-NQ4MtcRIog66wziD8fA"
 
   async function getData() {
-    const data = await ustube.getChannelVideos(chanelId)
-    console.log(data)
-  }
 
+    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${chanelId}&maxResults=250&order=date&key=${apikey}`, {
+        "method": "GET",});
+        const data = await response.json(); 
+        setVideos(data.items);
+
+        const response1 = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${chanelId}&key=${apikey}`, {
+          "method": "GET",});
+          const data1 = await response1.json(); 
+         setChanellogo(data1.items[0].snippet.thumbnails.medium.url);
+         setChanelDiscription(data1.items[0].snippet);
+  }
   return (
     <>
     <section className='home'>
       <div className='header'>
       <div className='left'>
-        <img src={chanelLogo} className='chanelLogo' alt='chanelLogo'></img>
-        <h2 className='chanelName'>{`${chanelName}`}</h2>  
+        <img src={chanellogo} className='chanelLogo' alt='chanelLogo'></img>
+        <h2 className='chanelName'>{`${chanelDiscription.title}`}</h2>  
       </div>
-      <div className='right'>
-        <button className='account'>L</button>
-      </div>
+      <div className='right'></div>
       </div>
       <div className='aboutWrapper'>
-        <h3 className='about'>{`${chanelDiscription}`}</h3>
+        <h3 className='about'>{`${chanelDiscription.description}`}</h3>
       </div>
     </section>
     <section className='video'>
