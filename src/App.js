b00ee432ded6a30d3f6193f0986a2fcd384106af
lpramord <lpramord@gmail.com>
2022-06-15 17:44:48 +0530

    as

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React  from 'react';
+import React , {useEffect, useState }  from 'react';
+import ustube from "usetube"
 import './App.css';
-import logo from'./pp.png';
 
-function App() {
+const App = () => {
+  
+  const [videos , setVideos] = useState([])
+  const [chanelDiscription , setChanelDiscription] = useState([])
+  const [chanelName , setChaneName] = useState([])
+  const [chanelLogo , setChanelLogo] = useState([])
+
+  useEffect(()=>{
+    getData()
+  },[])
+
+  const apikey = "AIzaSyAYjn6lsRnlUcQl4TO9Zi52nHlpqodA_sU"
+  const chanelId = "UCJA-NQ4MtcRIog66wziD8fA"
+
+  async function getData() {
+    const data = await ustube.getChannelVideos(chanelId)
+    console.log(data)
+  }
+
   return (
     <>
     <section className='home'>
       <div className='header'>
       <div className='left'>
-        <img src={logo} className='chanelLogo' alt='chanelLogo'></img>
-        <h2 className='chanelName'>eStory</h2>  
+        <img src={chanelLogo} className='chanelLogo' alt='chanelLogo'></img>
+        <h2 className='chanelName'>{`${chanelName}`}</h2>  
       </div>
       <div className='right'>
         <button className='account'>L</button>
       </div>
       </div>
       <div className='aboutWrapper'>
-        <h3 className='about'>dlsdklskflaskdsldkflsdkflsdkflsdk</h3>
+        <h3 className='about'>{`${chanelDiscription}`}</h3>
       </div>
     </section>
-    <section className='video'></section>
+    <section className='video'>
+      {videos.map((item)=>(
+        <iframe width="560" height="315" src={`https://www.youtube.com/embed/${item.id.videoId}`} key={`${item.id.videoId}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      ))}
+    </section>
     </>
   );
 }
