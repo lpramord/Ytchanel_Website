50673accf347c9dbb91fb43f0331bb9b6ad8c132
lpramord <lpramord@gmail.com>
2022-06-24 19:44:28 +0530

    a

--- src/App.js
+++ src/App.js
@@ -18,8 +18,16 @@ const App = () => {
   async function getData() {
     const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${chanelId}&maxResults=250&order=date&key=${apikey}`, {
       "method": "GET",});
-    const data = await response.json(); 
-    setVideos(data.items);
+    const data = await response.json();
+    const data2 = []
+    data.items.forEach(element => {
+      if(element.id.videoId===undefined){
+
+      }else{
+        data2.push(element)
+      }
+    });
+    setVideos(data2);
 
     const response1 = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${chanelId}&key=${apikey}`, {
       "method": "GET",});
@@ -27,12 +35,18 @@ const App = () => {
     setChanellogo(data1.items[0].snippet.thumbnails.medium.url);
     setChanelDiscription(data1.items[0].snippet);
   }
+
   return (
     <>
     <Helmet>
       <meta charSet="utf-8" />
       <title>{`${chanelDiscription.title}`}</title>
       <link rel="icon" type="image/x-icon" href={`${chanellogo}`}></link>
+      <meta property="og:title" content={`${chanelDiscription.title}`} />
+      <meta property="og:description" content={`${chanelDiscription.description}`} />
+      <meta property="og:image" content={chanellogo} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
     </Helmet>
     <section className='home'>
       <div className='header'>
